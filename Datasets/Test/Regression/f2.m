5e6a8f928020c022b1e3b01721474a7f0c6833ac
xapharius <mihai.suteu@xapharius.com>
2014-03-24 11:14:06 +0100

    added options to normalize input/output data + plot

--- Datasets/Test/Regression/f2.m
+++ Datasets/Test/Regression/f2.m
@@ -1,3 +1,3 @@
 function ret = f2(x)
-    ret = x.*x + 2*x - 1;
+    ret = x.*x - 2*x + 1;
 end
--- Datasets/Test/Regression/generateTrainingData.m
+++ Datasets/Test/Regression/generateTrainingData.m
@@ -1,18 +1,28 @@
 # _sampleSize   = Size of wanted training Set
-# _func         = function out of which to sample       (ex call "generateTrainingData(100, @f1)")
+# _func         = function out of which to sample       (ex call "generateTrainingData(@f1, 100)")
+# _sampleRange  = distance between min value and max value (ex range 2 => original data between [-1,1])
 
-function ret = generateTrainingData(_func, _sampleSize, _sampleRange)
-    data = (rand(_sampleSize, 1)-0.5) * _sampleRange;
-    data = [data, _func(data)];
-    #normalize
-    data = normalizePerCol(data);
-    dlmwrite("trainingData",data);
+function ret = generateTrainingData(_func, _sampleSize = 1000, _sampleRange = 4)
+
+    # TODO: generate inputs in a series so that y-plot-only makes sense
+    inputs = (rand(_sampleSize, 1)-0.5) * _sampleRange;
+    targets = _func(inputs);
+    
+    #normalize inputs
+    inputs = normalizePerCol(inputs);
+    #normalize targets
+    #targets = normalizePerCol(targets);
+
+    #plot(inputs', targets', ".");
+
+    data = [inputs, targets];
+    dlmwrite("trainingData",data);  
 end
 
 function ret = normalizePerCol(_mat)
-    for i = 1:size(_mat)(2)
-    maxCol = max(abs(_mat))(i);
-    _mat(:, i) = _mat(:, i)/maxCol;
     ret = _mat;
+    for i = 1:size(_mat)(2)
+        ret(:,i) = _mat(:,i)/norm(_mat(:,i));
+    end
 end
 
